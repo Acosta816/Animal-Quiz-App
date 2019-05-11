c7b5a2c2053d36990dbea0d101b07564c9fd20e6
David Acosta <acosta816@gmail.com>
2019-05-11 14:18:00 -0700

    start point for ISAAC

--- index.js
+++ index.js
@@ -1,6 +1,54 @@
 let questionNumber = 0;
 let score = 0;
 
+console.log('anything ');
+
+$('.js-start-button').on('click', event=>{
+
+    console.log(event);
+
+    $('.js-question-answer-form').html(generateQuestion());
+    $('.js-quiz-start').hide();
+});
+
+function displayFeedback(userAnswer){
+    if(userAnswer === STORE[questionNumber].correctAnswer){
+        return `<div>
+                    <h2>You Got it RIGHT!</h2>
+                    <p>correct answer: ${STORE[questionNumber].correctAnswer}</p>
+                </div>`;
+    }
+}
+
+
+$('#myForm input').on('change', function() {
+    alert($('input[name=radioName]:checked', '#myForm').val()); 
+ });
+
+
+
+
+
+$('.js-question-answer-form').on('submit', 'input', event => {
+    event.preventDefault();
+    console.log(event);
+
+    let answerPicked = getUserAnswer();
+
+    displayFeedback(answerPicked);
+
+    //  displayFeedback(event.CurrentTarget.)
+    /*if userchoice ==== question.correctAnswer{
+        call display CORRECT feedback div function}
+        else {
+         call display WRONG feedback div w/correct answer
+        }
+    } */
+
+})
+
+
+
 //this will generate the htmlfor the questions, basically the view
 function generateQuestion() {
     if(questionNumber < STORE.length){ //accessing question object by STORE index using the questionNumber as the cycler/iterator
@@ -24,7 +72,7 @@ function generateQuestion() {
         <input type="radio" value="${STORE[questionNumber].answers[3]}" name="answer" required>
         <span>${STORE[questionNumber].answers[3]}</span>
         </label>
-        <button type="submit" class="submitButton">Submit</button>
+        <button type="submit" class="js-submit-button">Submit</button>
         </fieldset>
         </form>
         </div>`;
